bef32afd9fabfb93bf17cf187b031f346af8da17
RockBrentwood <RockBrentwood@gmail.com>
2016-09-10 16:20:28 -0500

    Add files via upload

--- Source/Version.h
+++ Source/Version.h
@@ -1,4 +1,4 @@
-#define CBC_VERSION "cbc version 1.2"
+#define CBC_VERSION "cbc version 1.4"
 
 // Used for the local version of C-BC. Already defined in the makefile.
 #define MATH_LIB "/usr/local/lib/math.b"
